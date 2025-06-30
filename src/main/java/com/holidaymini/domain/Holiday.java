52db39d6c1f871626ec2bb0953dfe40354e7c29b
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-06-30 22:57:06 +0900

    feat(Holiday): 휴일 클래스 및 Embed 생성

---
+++ src/main/java/com/holidaymini/domain/Holiday.java
@@ -0,0 +1,88 @@
+package com.holidaymini.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(indexes = {
+        @Index(name = "idx_holiday_country_year", columnList = "country_id, holiday_year"),
+        @Index(name = "idx_holiday_date", columnList = "date"),
+        @Index(name = "idx_holiday_country_date", columnList = "country_id, holiday_date")
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Holiday extends BaseTimeEntity {
+
+    private static final LocalDate LOWER_DATE_BOUND = LocalDate.of(2020, 1, 1);
+    private static final LocalDate UPPER_DATE_BOUND = LocalDate.of(2025, 12, 31);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "holiday_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "holiday_year", nullable = false)
+    private Integer year;
+
+    @Column(name = "holiday_date", nullable = false)
+    private LocalDate date;
+
+    @Embedded
+    private HolidayDetail detail;
+
+    public Holiday(Country country, LocalDate date, String name, HolidayDetail detail) {
+        this.country = country;
+        this.date = date;
+        this.name = name;
+        this.year = date.getYear();
+        this.detail = detail;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Holiday holiday)) {
+            return false;
+        }
+        return Objects.equals(id, holiday.id) && Objects.equals(country, holiday.country)
+                && Objects.equals(year, holiday.year) && Objects.equals(date, holiday.date)
+                && Objects.equals(name, holiday.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, country, year, date, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Holiday{" +
+                "id=" + id +
+                ", date=" + date +
+                ", localName='" + detail.getLocalName() + '\'' +
+                ", name='" + name + '\'' +
+                ", countryCode='" + country.getName() + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/main/java/com/holidaymini/domain/HolidayDetail.java
@@ -0,0 +1,50 @@
+package com.holidaymini.domain;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import java.util.Set;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HolidayDetail {
+
+    @Column(nullable = false)
+    private String localName;
+
+    @Column(nullable = false)
+    private Boolean isFixed;
+
+    @Column(nullable = false)
+    private Boolean isGlobal;
+
+    private Integer launchYear;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "holiday_types", joinColumns = @JoinColumn(name = "holiday_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<HolidayType> types;
+
+    public HolidayDetail(
+            Boolean isFixed,
+            Boolean isGlobal,
+            Integer launchYear,
+            String localName,
+            Set<HolidayType> types
+    ) {
+        this.isFixed = isFixed;
+        this.isGlobal = isGlobal;
+        this.launchYear = launchYear;
+        this.localName = localName;
+        this.types = types;
+    }
+}
