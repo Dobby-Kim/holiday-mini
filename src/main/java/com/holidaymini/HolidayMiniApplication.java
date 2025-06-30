2f97734bd1f050068505dc5293f2fede7e190d75
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2025-06-30 15:03:09 +0900

    chore: init setting

---
+++ src/main/java/com/holidaymini/HolidayMiniApplication.java
@@ -0,0 +1,12 @@
+package com.holidaymini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayMiniApplication.class, args);
+    }
+}
---
+++ src/test/java/com/holidaymini/HolidayMiniApplicationTests.java
@@ -0,0 +1,12 @@
+package com.holidaymini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
